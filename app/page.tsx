35675b44601954f6fe4f9b5758c56721a64d1ccb
DavidQ <gerenciamueblessisbetter@gmail.com>
2026-05-01 16:19:31 -0400

    agragando imagen portada

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home(){
-  return(<div><h1>Hola Mundo</h1></div>)
+  return(<div><h1>Muebless is Better</h1>
+    <img
+    src = "/portada.jpeg"
+    alt = "Muebless is Better"
+    style ={{width: "100%", height: "auto"}}
+    />
+    </div>)
 }
